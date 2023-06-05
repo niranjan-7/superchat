a4e8f70c03d17d249ba0d4872a021d63a28b0033
niranjan-7 <86029534+niranjan-7@users.noreply.github.com>
2023-06-05 16:13:42 +0530

    send

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Enter Message" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>send</button>
 
     </form>
   </>)
